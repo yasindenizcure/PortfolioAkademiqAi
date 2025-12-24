208f8de17f310049c1232b86b966a6069639e4e1
yasindenizcure <yasindenizcure@gmail.com>
2025-12-24 20:59:55 +0300

    İstatistiklerin oluşturulması.

---
+++ Controllers/StatisticsController.cs
@@ -0,0 +1,34 @@
+﻿using AkademiQPortfolio.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQPortfolio.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public StatisticsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult StatisticsCards()
+        {
+            var messageCount= _context.Messages.Count();
+            var messageCountByIsReadTrue = _context.Messages.Where(x=> x.IsRead == true).Count();
+            var messageCountByIsReadFalse = _context.Messages.Where(x=> x.IsRead == false).Count();
+            var skillCount = _context.Skills.Count();
+            var skillAvgValue = _context.Skills.Average(x=> x.SkillValue);
+            var skillValueBiggerThan70= _context.Skills.Where(x=> x.SkillValue >= 70).Count();
+
+            ViewBag.messageCount = messageCount;
+            ViewBag.messageCountByIsReadTrue = messageCountByIsReadTrue;
+            ViewBag.messageCountByIsReadFalse = messageCountByIsReadFalse;
+            ViewBag.skillCount = skillCount;
+            ViewBag.skillAvgValue = skillAvgValue;
+            ViewBag.skillValueBiggerThan70 = skillValueBiggerThan70;
+
+            return View();
+        }
+    }
+}
